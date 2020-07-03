a8ae0c9a451b47c98900f3057363c6d4492d9d79
nebulamagic <892461474@qq.com>
2020-07-03 10:49:40 +0800

    目录整理 添加说明

---
+++ 18-20-LTDC—液晶显示/20-LTDC—液晶显示汉字（显示任意大小）/User/main.c
@@ -0,0 +1,319 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2017-xx-xx
+  * @brief   GPIO输出--使用固件库点亮LED灯
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32 F429 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx.h"
+#include "./usart/bsp_debug_usart.h" 
+#include "./sdram/bsp_sdram.h"
+#include <stdlib.h>
+#include "./led/bsp_led.h" 
+#include "./lcd/bsp_lcd.h"
+#include "./flash/bsp_spi_flash.h"
+
+void Delay(__IO uint32_t nCount); 
+
+void LCD_Test(void);
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+    /* 系统时钟初始化成216 MHz */
+    SystemClock_Config();
+    /* LED 端口初始化 */
+    LED_GPIO_Config();	 
+    /* LCD 端口初始化 */ 
+    LCD_Init();
+    /* LCD 第一层初始化 */ 
+    LCD_LayerInit(0, LCD_FB_START_ADDRESS,RGB565);
+	/* LCD 第二层初始化 */ 
+    LCD_LayerInit(1, LCD_FB_START_ADDRESS+(LCD_GetXSize()*LCD_GetYSize()*4),ARGB8888);
+    /* 使能LCD，包括开背光 */ 
+    LCD_DisplayOn(); 
+
+    /* 选择LCD第一层 */
+    LCD_SelectLayer(0);
+
+    /* 第一层清屏，显示全黑 */ 
+    LCD_Clear(LCD_COLOR_BLACK);  
+
+    /* 选择LCD第二层 */
+    LCD_SelectLayer(1);
+
+    /* 第二层清屏，显示全黑 */ 
+    LCD_Clear(LCD_COLOR_TRANSPARENT);
+
+    /* 配置第一和第二层的透明度,最小值为0，最大值为255*/
+    LCD_SetTransparency(0, 255);
+    LCD_SetTransparency(1, 0);
+	
+    /* 选择LCD第一层 */
+    LCD_SelectLayer(0);
+	
+	/* 清屏，显示全黑 */
+	LCD_Clear(LCD_COLOR_BLACK);	
+	/*设置字体颜色及字体的背景颜色(此处的背景不是指LCD的背景层！注意区分)*/
+	LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_BLACK);
+	/*选择字体*/
+	LCD_SetFont(&LCD_DEFAULT_FONT);
+
+    while(1)
+    {
+      LCD_Test(); 
+    }
+}
+
+/*用于测试各种液晶的函数*/
+void LCD_Test(void)
+{
+	/*演示显示变量*/
+	static uint8_t testCNT = 0;	
+	char dispBuff[100];
+	
+    /* 选择LCD第一层 */
+    LCD_SelectLayer(0);
+	
+	/* 清屏，显示全黑 */
+	LCD_Clear(LCD_COLOR_BLACK);	
+	/*设置字体颜色及字体的背景颜色(此处的背景不是指LCD的背景层！注意区分)*/
+	LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_BLACK);
+	/*选择字体*/
+	LCD_SetFont(&LCD_DEFAULT_FONT);
+
+	LCD_DisplayStringLineEx(0,5,16,16,(uint8_t* )"野火F429 16*16 ",0);
+    LCD_DisplayStringLine_EN_CH(1,(uint8_t* )"野火F429 24*24 ");
+    LCD_DisplayStringLineEx(0,50,32,32,(uint8_t* )"野火F429 32*32 ",0);
+    LCD_DisplayStringLineEx(0,82,48,48,(uint8_t* )"野火F429 48*48 ",0);
+
+    /*使用c标准库把变量转化成字符串*/
+    sprintf(dispBuff,"显示变量例子: testCount = %d ",testCNT);
+	LCD_ClearLine(7);
+	/*设置字体颜色及字体的背景颜色(此处的背景不是指LCD的背景层！注意区分)*/
+	LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_BLACK);
+	/*然后显示该字符串即可，其它变量也是这样处理*/
+	LCD_DisplayStringLine_EN_CH(7,(uint8_t* )dispBuff);
+
+
+	/* 画直线 */
+	LCD_SetTextColor(LCD_COLOR_BLUE);
+
+	LCD_ClearLine(8);
+    LCD_DisplayStringLine_EN_CH(8,(uint8_t* )"画线:");
+	LCD_SetColors(LCD_COLOR_RED,LCD_COLOR_BLACK);
+	LCD_DrawLine(50,250,750,250);  
+	LCD_DrawLine(50,300,750,300);
+
+	LCD_SetColors(LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+	LCD_DrawLine(300,250,400,400);  
+	LCD_DrawLine(600,250,600,400);
+
+	Delay(0xFFFFFF);
+
+	LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_BLACK);
+	LCD_FillRect(0,200,LCD_PIXEL_WIDTH,LCD_PIXEL_HEIGHT-200);
+
+
+	/*画矩形*/
+	LCD_SetTextColor(LCD_COLOR_BLUE);
+	LCD_ClearLine(8);
+    LCD_DisplayStringLine_EN_CH(8,(uint8_t* )"画矩形:");
+	
+	LCD_SetColors(LCD_COLOR_RED,LCD_COLOR_BLACK);
+	LCD_DrawRect(200,250,200,100);
+
+	LCD_SetColors(LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+	LCD_DrawRect(350,250,200,50);
+
+	LCD_SetColors(LCD_COLOR_BLUE,LCD_COLOR_BLACK);
+	LCD_DrawRect(200,350,50,200);
+
+	Delay(0xFFFFFF);
+
+
+	LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_BLACK);
+	LCD_FillRect(0,200,LCD_PIXEL_WIDTH,LCD_PIXEL_HEIGHT-200);
+
+
+	/*填充矩形*/
+	LCD_SetTextColor(LCD_COLOR_BLUE);
+	LCD_ClearLine(8);
+    LCD_DisplayStringLine_EN_CH(8,(uint8_t* )"填充矩形:");
+
+	LCD_SetColors(LCD_COLOR_RED,LCD_COLOR_BLACK);
+	LCD_FillRect(200,250,200,100);
+
+	LCD_SetColors(LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+	LCD_FillRect(350,250,200,50);
+
+	LCD_SetColors(LCD_COLOR_BLUE,LCD_COLOR_BLACK);
+	LCD_FillRect(200,350,50,200);
+
+	Delay(0xFFFFFF);
+
+	LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_BLACK);
+	LCD_FillRect(0,200,LCD_PIXEL_WIDTH,LCD_PIXEL_HEIGHT-200);
+
+	/* 画圆 */
+	LCD_SetTextColor(LCD_COLOR_BLUE);
+	LCD_ClearLine(8);
+    LCD_DisplayStringLine_EN_CH(8,(uint8_t* )"画圆:");
+
+	LCD_SetColors(LCD_COLOR_RED,LCD_COLOR_RED);
+	LCD_DrawCircle(200,350,50);
+
+	LCD_SetColors(LCD_COLOR_GREEN,LCD_COLOR_GREEN);
+	LCD_DrawCircle(350,350,75);
+
+	Delay(0xFFFFFF);
+
+	LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_BLACK);
+	LCD_FillRect(0,200,LCD_PIXEL_WIDTH,LCD_PIXEL_HEIGHT-200);
+
+
+	/*填充圆*/
+	LCD_SetTextColor(LCD_COLOR_BLUE);
+	LCD_ClearLine(8);
+    LCD_DisplayStringLine_EN_CH(8,(uint8_t* )"填充圆:");
+
+	LCD_SetColors(LCD_COLOR_RED,LCD_COLOR_BLACK);
+	LCD_FillCircle(300,350,50);
+
+	LCD_SetColors(LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+	LCD_FillCircle(450,350,75);
+
+	Delay(0xFFFFFF);
+
+	LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_BLACK);
+	LCD_FillRect(0,200,LCD_PIXEL_WIDTH,LCD_PIXEL_HEIGHT-200);
+
+	LCD_ClearLine(8);
+	
+	/*透明效果 前景层操作*/
+	LCD_SetTextColor(LCD_COLOR_BLUE);
+	LCD_ClearLine(8);
+	LCD_DisplayStringLine_EN_CH(8,(uint8_t* )"双层透明效果：");
+	
+	/*设置前景层不透明度*/
+	LCD_SetTransparency(1, 128);
+
+    /* 选择LCD第一层 */
+    LCD_SelectLayer(1);
+	
+	/* 清屏，显示全黑 */
+	LCD_Clear(LCD_COLOR_BLACK);	
+	/*在前景画一个红色圆*/
+	LCD_SetColors(LCD_COLOR_RED,LCD_COLOR_TRANSPARENT);
+	LCD_FillCircle(400,350,75);
+	
+	Delay(0xFFFFFF);
+	
+	/*透明效果 背景层操作*/
+
+	/* 选择LCD背景层 */
+	LCD_SelectLayer(0);	
+	LCD_Clear(LCD_COLOR_BLACK);		
+	/*设置背景层不透明*/
+	LCD_SetTransparency(1, 0);
+	
+
+	/*在背景画一个绿色圆*/
+	LCD_SetColors(LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+	LCD_FillCircle(450,350,75);
+	
+	/*在背景画一个蓝色圆*/
+	LCD_SetColors(LCD_COLOR_BLUE,LCD_COLOR_BLACK);
+	LCD_FillCircle(350,350,75);
+	
+	Delay(0xFFFFFF);
+	LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_BLACK);
+	LCD_FillRect(0,200,LCD_PIXEL_WIDTH,LCD_PIXEL_HEIGHT-200);
+}
+
+
+/**
+  * @brief  系统时钟配置 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 180000000
+  *            HCLK(Hz)                       = 180000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 360
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  无
+  * @retval 无
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* 使能HSE，配置HSE为PLL的时钟源，配置PLL的各种分频因子M N P Q 
+	 * PLLCLK = HSE/M*N/P = 25M / 25 *432 / 2 = 216M
+	 */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+   while(1) {}
+  }
+
+  /* 激活 OverDrive 模式 */
+  HAL_PWREx_EnableOverDrive();
+ 
+  /* 选择PLLCLK作为SYSCLK，并配置 HCLK, PCLK1 and PCLK2 的时钟分频因子 
+	 * SYSCLK = PLLCLK     = 180M
+	 * HCLK   = SYSCLK / 1 = 180M
+	 * PCLK2  = SYSCLK / 2 = 90M
+	 * PCLK1  = SYSCLK / 4 = 45M
+	 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    while(1) {}
+  }
+}
+
+void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
