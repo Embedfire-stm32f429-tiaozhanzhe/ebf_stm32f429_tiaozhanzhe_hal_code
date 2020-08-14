0817c9252e16e704edeca45b084b2f0e1c31c4cd
hejiawen <hjw0415@outlook.com>
2020-08-14 16:54:02 +0800

    增加stc文件应用例程

---
+++ MDK编译过程及文件全解/SCT文件应用/1.自动分配变量到SDRAM/User/main.c
@@ -0,0 +1,170 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2017-xx-xx
+  * @brief   GPIO输出--使用固件库点亮LED灯
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32 F429 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx.h"
+#include "./usart/bsp_debug_usart.h" 
+#include "./sdram/bsp_sdram.h"
+#include <stdlib.h>
+#include "./led/bsp_led.h" 
+
+void Delay(__IO uint32_t nCount); 
+
+//定义变量到SDRAM
+uint32_t testValue  =0 ;
+//定义变量到SDRAM
+uint32_t testValue2  =7;
+
+//定义数组到SDRAM
+uint8_t testGrup[100]  ={0};
+//定义数组到SDRAM
+uint8_t testGrup2[100] ={1,2,3};
+
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+	uint32_t inerTestValue =10;
+	
+	/* 系统时钟初始化成216 MHz */
+	SystemClock_Config();
+	/* LED 端口初始化 */
+	LED_GPIO_Config();	 
+	/* 初始化串口 */
+	DEBUG_USART_Config();
+
+	printf("\r\nSCT文件应用——自动分配变量到SDRAM实验\r\n");
+
+	printf("\r\n使用“	uint32_t inerTestValue =10; ”语句定义的局部变量：\r\n");
+	printf("结果：它的地址为：0x%x,变量值为：%d\r\n",(uint32_t)&inerTestValue,inerTestValue);
+
+	printf("\r\n使用“uint32_t testValue  =0 ;”语句定义的全局变量：\r\n");
+	printf("结果：它的地址为：0x%x,变量值为：%d\r\n",(uint32_t)&testValue,testValue);
+
+	printf("\r\n使用“uint32_t testValue2  =7 ; ”语句定义的全局变量：\r\n");
+	printf("结果：它的地址为：0x%x,变量值为：%d\r\n",(uint32_t)&testValue2,testValue2);
+
+	printf("\r\n使用“uint8_t testGrup[100]  ={0};”语句定义的全局数组：\r\n");
+	printf("结果：它的地址为：0x%x,变量值为：%d,%d,%d\r\n",(uint32_t)&testGrup,testGrup[0],testGrup[1],testGrup[2]);
+
+	printf("\r\n使用“uint8_t testGrup2[100] ={1,2,3};”语句定义的全局数组：\r\n");
+	printf("结果：它的地址为：0x%x,变量值为：%d，%d,%d\r\n",(uint32_t)&testGrup2,testGrup2[0],testGrup2[1],testGrup2[2]);
+
+	
+	/*使用malloc动态分配变量*/
+	
+	uint32_t * pointer = (uint32_t*)malloc(sizeof(uint32_t)*3); 
+	if(pointer != NULL)
+	{
+		*(pointer)=1;
+		*(++pointer)=2;
+		*(++pointer)=3;	
+		
+		printf("\r\n使用“	uint32_t *pointer = (uint32_t*)malloc(sizeof(uint32_t)*3); ”动态分配的变量\r\n");
+		printf("\r\n定义后的操作为：\r\n*(pointer++)=1;\r\n*(pointer++)=2;\r\n*pointer=3;\r\n\r\n");
+		printf("结果：操作后它的地址为：0x%x,查看变量值操作：\r\n",(uint32_t)pointer); 
+		printf("*(pointer--)=%d, \r\n",*(pointer--));
+		printf("*(pointer--)=%d, \r\n",*(pointer--));
+		printf("*(pointer)=%d, \r\n",*(pointer));
+		
+		free(pointer);
+	}
+	else
+	{
+		printf("\r\n使用malloc动态分配变量出错！！！\r\n");	
+	}
+  /*蓝灯亮，表示正在读写SDRAM测试*/
+  LED_BLUE;
+
+  while(1); 	
+}
+
+/**
+  * @brief  系统时钟配置 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 180000000
+  *            HCLK(Hz)                       = 180000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 360
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  无
+  * @retval 无
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* 使能HSE，配置HSE为PLL的时钟源，配置PLL的各种分频因子M N P Q 
+	 * PLLCLK = HSE/M*N/P = 25M / 25 *432 / 2 = 216M
+	 */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+   while(1) {}
+  }
+
+  /* 激活 OverDrive 模式 */
+  HAL_PWREx_EnableOverDrive();
+ 
+  /* 选择PLLCLK作为SYSCLK，并配置 HCLK, PCLK1 and PCLK2 的时钟分频因子 
+	 * SYSCLK = PLLCLK     = 180M
+	 * HCLK   = SYSCLK / 1 = 180M
+	 * PCLK2  = SYSCLK / 2 = 90M
+	 * PCLK1  = SYSCLK / 4 = 45M
+	 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    while(1) {}
+  }
+}
+
+void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
